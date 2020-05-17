3ca498f38c8f687a0cbb18def2c70a5714b2a950
arunxxxl <arunberry47@gmail.com>
2020-05-17 22:58:27 +0530

    commiting on: Sun 17 May 22:58:27 IST 2020

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 const app = new Vue({
     el: "#app",
     data: {
-        sectionOne: 'hello',
+        sectionOne: 'contact',
         windowHeight: 0,
         openNav: false,
         contact: {
