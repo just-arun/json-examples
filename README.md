0aaafda45211d1012375fcdb3fe1e13eddf7ae27
arunxxxl <arunberry47@gmail.com>
2020-05-17 14:44:41 +0530

    adding about page

--- README.md
+++
@@ -1,3 +0,0 @@
-# json-examples
-
-this page contains example files for JSON/AJAX tutorial in [learn-js.quedev.dev](https://learn-js.quedev.dev)
---
+++ js/main.js
@@ -0,0 +1,33 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        sectionOne: false,
+        windowHeight: 0
+    },
+    mounted() {
+        window.addEventListener('scroll', this.handleScroll);
+        window.addEventListener("hashchange", this.handleSectionChange, false);
+        this.windowHeight = window.innerHeight
+    },
+    methods: {
+        handleScroll(e) {
+            if (window.pageYOffset > 50) {
+                location.hash = "#about"
+            }
+            if (window.pageYOffset < 45) {
+                location.hash = "#"
+            }
+        },
+        handleSectionChange() {
+            console.log(location.hash);
+            switch (location.hash) {
+                case "":
+                    this.sectionOne = false
+                    break;
+                case "#about":
+                    this.sectionOne = true
+                    break;
+            }
+        }
+    }
+});
